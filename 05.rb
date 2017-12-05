55c73739850dc9f34dfa050d7aca9a99675f3480
John Hawthorn <john.hawthorn@gmail.com>
2017-12-04 21:07:45 -0800

    Day 5 part 2

--- 05.rb
+++ 05.rb
@@ -7,9 +7,14 @@ pc = 0
 steps = 0
 loop do
   break if pc < 0 || pc >= data.length
-  p pc
-  nextpc = pc + data[pc]
-  data[pc] += 1
+  oldpc = pc
+  offset = data[pc]
+  nextpc = pc + offset
+  if offset >= 3
+    data[pc] -= 1
+  else
+    data[pc] += 1
+  end
   pc = nextpc
   steps += 1
 end
