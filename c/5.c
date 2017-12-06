d7d9e6a8c5ab4c6e90666689dd41095b78fce0cb
John Hawthorn <john.hawthorn@gmail.com>
2017-12-05 18:12:36 -0800

    Even hackier C solution

--- c/5.c
+++ c/5.c
@@ -3,15 +3,15 @@
 #include "5.h"
 
 int main() {
-	int pc = 0;
+	int *pc = mem;
 	int iterations = 0;
 
-	for(; pc >= 0 && pc < 1059; iterations++) {
-		int offset = mem[pc];
+	for(; pc >= mem && pc < mem + (sizeof(mem) / sizeof(mem[0])); iterations++) {
+		int offset = *pc;
 		if (offset >= 3)
-			mem[pc]--;
+			(*pc)--;
 		else
-			mem[pc]++;
+			(*pc)++;
 
 		pc += offset;
 	}
