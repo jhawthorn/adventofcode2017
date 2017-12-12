fe8dbcdc3818368c6223032bd6ec40ae1e4b6c1e
John Hawthorn <john.hawthorn@gmail.com>
2017-12-11 21:30:44 -0800

    Day 10 part 2

--- 10.rb
+++ 10.rb
@@ -1,24 +1,50 @@
-data = File.read("data/10.txt").strip.split(",").map(&:to_i)
+data = File.read("data/10.txt").strip
 
-def calculate(list_size, lengths)
+def hash_round(list_size, original_lengths, rounds: 1)
   list = 0.upto(list_size-1).to_a
   skip = 0
   position = 0
 
-  while !lengths.empty?
-    length = lengths.shift
 
-    list[0,length] = list[0,length].reverse
+  rounds.times do
+    lengths = original_lengths.dup
+    while !lengths.empty?
+      length = lengths.shift
 
-    position += length + skip
-    list.rotate!(length + skip)
+      list[0,length] = list[0,length].reverse
 
-    skip += 1
+      position += length + skip
+      list.rotate!(length + skip)
+
+      skip += 1
+    end
   end
 
   list.rotate!(-position)
+
+  list
+end
+
+def part1(list_size, input)
+  list = hash_round(list_size, input)
   list[0] * list[1]
 end
 
-p calculate(5, [3,4,1,5])
-p calculate(256, data)
+raise unless part1(5, [3,4,1,5]) == 12
+part1_data = data.split(',').map(&:to_i)
+p part1(256, part1_data)
+
+def part2(input)
+  lengths = input.chars.map(&:ord) + [17, 31, 73, 47, 23]
+  list = hash_round(256, lengths, rounds: 64)
+
+  list.each_slice(16).map{|x| "%.2x" % x.inject(:^) }.join
+end
+
+#p part2_data
+raise unless part2("") == "a2582a3a0e66e6e86e3812dcb672a272"
+raise unless part2("AoC 2017")
+p part2("1,2,3")
+p part2("1,2,4")
+
+p part2(data)
