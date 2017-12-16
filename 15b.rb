0fb3f5286e07ea22421118eca7bad222d798ebf4
John Hawthorn <john.hawthorn@gmail.com>
2017-12-15 16:10:10 -0800

    Make 15b slightly faster

--- 15b.rb
+++ 15b.rb
@@ -1,21 +1,9 @@
 def calculate(a, b)
-  as = Enumerator.new do |y|
-    loop do
-      a = a * 16807 % 2147483647
-      y << a if a % 4 == 0
-    end
-  end
-
-  bs = Enumerator.new do |y|
-    loop do
-      b = b * 48271 % 2147483647
-      y << b if b % 8 == 0
-    end
-  end
-
   5_000_000.times.count do
-    a = as.next
-    b = bs.next
+    a = a * 16807 % 2147483647
+    a = a * 16807 % 2147483647 while a % 4 != 0
+    b = b * 48271 % 2147483647
+    b = b * 48271 % 2147483647 while b % 8 != 0
 
     (a & 0xffff) == (b & 0xffff)
   end
