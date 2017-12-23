05d4815995559b90fd3ef908bea3380ffd427234
John Hawthorn <john.hawthorn@gmail.com>
2017-12-22 22:00:01 -0800

    Day 23b: golf it

--- 23b.rb
+++ 23b.rb
@@ -1,21 +1,2 @@
-require 'pry'
-require 'byebug'
 require 'prime'
-a = b = c = d = e = f = h = 0
-a = 1
-b = 67
-c = b
-#jmp 2 if a != 0
-#jmp 5
-b *= 100
-b += 100000
-c = b
-c += 17000
-loop do
-  h += 1 unless b.prime?
-  if b - c == 0
-    puts h
-    exit 0
-  end
-  b += 17
-end
+p (106700..123700).step(17).count{|x|!x.prime?}
