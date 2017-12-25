3b66d91874554fff0586c622081e3cb44576be82
John Hawthorn <john.hawthorn@gmail.com>
2017-12-24 20:17:55 -0800

    Cleanup output of Day 8

--- 08.rb
+++ 08.rb
@@ -1,10 +1,14 @@
 data = File.read("data/08.txt")
 
+after_each = <<RUBY
+max = [max, vars.values.max || 0].max
+RUBY
+
 vars = Hash.new(0)
 max = 0
 data = data.gsub('dec', '-=').gsub('inc', '+=').gsub(/(^| if )([a-z]+)/, "\\1vars['\\2']")
-data = data.lines.map{|x| x + "max = [max, vars.values.max || 0].max\n" }.join
-puts data
+data = data.lines.map{|x| x + "#{after_each}\n" }.join
+#puts data
 eval(data)
 p vars.values.max
 p max
