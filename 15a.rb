94b7ecae5d785a9ac2386cbaf7bdfc6993fc14d4
John Hawthorn <john.hawthorn@gmail.com>
2018-02-05 21:45:06 -0800

    MJIT test

--- 15a.rb
+++ 15a.rb
@@ -1,11 +1,17 @@
-def calculate(a, b)
-  40_000_000.times.count do
+def calculate(a, b, n=40_000_000)
+  i = 0; c = 0
+  while i < n
     a = a * 16807 % 2147483647
     b = b * 48271 % 2147483647
 
-    (a & 0xffff) == (b & 0xffff)
+    c += 1 if (a & 0xffff) == (b & 0xffff)
+
+    i += 1
   end
+  c
 end
+
+5.times { calculate(0,0,1) }
 
 raise unless calculate(65, 8921) == 588
 p calculate(699, 124)
